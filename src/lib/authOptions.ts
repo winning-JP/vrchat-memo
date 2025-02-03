4919447c755331e09aab253182fd69c71394ef6e
winningJP <github@winning.moe>
2025-02-03 11:53:02 +0900

    discord authを追加

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./prismadb";
 import GitHubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
@@ -9,6 +10,10 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET
+    })
   ],
   secret: process.env.NEXTAUTH_SECRET,
   session: {
